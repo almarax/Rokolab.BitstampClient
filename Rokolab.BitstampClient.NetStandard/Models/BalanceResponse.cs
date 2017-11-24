1fd01bec6262b4892ecb1a9756cac1de3cdb8142
Alexander Marakhovskiy <alexander.marakhovskiy@outlook.com>
2017-11-24 17:17:34 +0100

    copied to net standard template, dependencies resolved (with warnings)

---
+++ Rokolab.BitstampClient.NetStandard/Models/BalanceResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace Rokolab.BitstampClient.Models
+{
+    public class BalanceResponse
+    {
+        public string fee { get; set; }
+
+        //balance
+        public string usd_balance { get; set; }
+        public string eur_balance { get; set; }
+        public string btc_balance { get; set; }
+
+        //reserved
+        public string usd_reserved { get; set; }
+        public string btc_reserved { get; set; }
+        public string eur_reserved { get; set; }
+
+        //available
+        public string usd_available { get; set; }
+        public string btc_available { get; set; }
+        public string eur_available { get; set; }
+    }
+}
